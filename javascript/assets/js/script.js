a443c4030a47f51da8707069b30dccce4bdf47f7
songjunseop <97070078+seongwoo91@users.noreply.github.com>
2022-01-19 14:50:38 +0900

    22..01.19

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,6 +1,12 @@
 //결과보기 버튼을 클릭하면 경고창의 띄워주세요~ 
 document.querySelectorAll(".result").forEach(function(el){
     el.addEventListener("click", function(){
-        alert("dtd")
+       el.classList.toggle("active");
     })
 })
+
+// document.querySelectorAll(".result").forEach(function(el){
+//     el.addEventListener("click", function(){
+//         alert("ddd")
+//     })
+// })
