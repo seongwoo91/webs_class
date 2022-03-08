b8a9dac0b2fb6683586f74185d6511dda1f2bbd5
seongwoo91 <97070078+seongwoo91@users.noreply.github.com>
2022-03-08 22:28:27 +0900

    22.03.08

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,10 +1,10 @@
 
 //결과 보기 토글
-document.querySelectorAll(".result").forEach(function(el){
-    el.addEventListener("click", function(){
-        el.classList.toggle("active");
-    });
-});
+//document.querySelectorAll(".result").forEach(function(el){
+//    el.addEventListener("click", function(){
+//        el.classList.toggle("active");
+//    });
+//});
 
 document.querySelectorAll(".result").forEach(function(el){
     el.addEventListener("click", function(){
