63829acb9fa1a45e6486d06360c71d5bb6a150d9
seongwoo91 <97070078+seongwoo91@users.noreply.github.com>
2022-03-08 18:08:18 +0900

    22.03.08

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,12 +1,13 @@
-//결과보기 버튼을 클릭하면 경고창의 띄워주세요~ 
+
+//결과 보기 토글
 document.querySelectorAll(".result").forEach(function(el){
     el.addEventListener("click", function(){
-       el.classList.toggle("active");
-    })
-})
+        el.classList.toggle("active");
+    });
+});
 
-// document.querySelectorAll(".result").forEach(function(el){
-//     el.addEventListener("click", function(){
-//         alert("ddd")
-//     })
-// })
+document.querySelectorAll(".result").forEach(function(el){
+    el.addEventListener("click", function(){
+        el.classList.toggle("active");
+    });
+})
