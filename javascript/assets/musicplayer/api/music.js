d2f9d12d3e27e206c6f9672f98171ed3a1d403a1
seongwoo91 <seongwoo4477@gmail.com>
2022-04-07 12:59:28 +0900

    22.04.07

---
+++ javascript/assets/musicplayer/api/music.js
@@ -0,0 +1,62 @@
+let allMusic = [
+    {
+        name : "봄이여 오라(Feat.유리)",
+        artist : "MC스나이퍼",
+        img : "sound1",
+        audio : "sound1"
+    },
+    {
+        name : "다정히 내 이름을 부르면",
+        artist : "경서예지",
+        img : "sound2",
+        audio : "sound2"
+    },
+    {
+        name : "사실 나는 (Feat. 전건호)",
+        artist : "경서예지",
+        img : "sound3",
+        audio : "sound3"
+    },
+    {
+        name : "은하수를 닮은 너에게 (Feat. 몰리디)",
+        artist : "경서예지",
+        img : "sound4",
+        audio : "sound4"
+    },
+    {
+        name : "사랑의 바보",
+        artist : "더 넛츠",
+        img : "sound5",
+        audio : "sound5"
+    },
+    {
+        name : "사랑은 가슴이 시킨다",
+        artist : "버즈",
+        img : "sound6",
+        audio : "sound6"
+    },
+    {
+        name : "Love Love Love",
+        artist : "에픽하이",
+        img : "sound7",
+        audio : "sound7"
+    },
+    {
+        name : "선물",
+        artist : "UN",
+        img : "sound8",
+        audio : "sound8"
+    },
+    {
+        name : "흔들리는 꽃들 속에서 네 샴푸향이 느껴진거야",
+        artist : "장범준",
+        img : "sound9",
+        audio : "sound9"
+    },
+    {
+        name : "사랑은 향기를 남기고",
+        artist : "테이",
+        img : "sound10",
+        audio : "sound10"
+    }
+]
